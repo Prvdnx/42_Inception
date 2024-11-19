180092aac7ea4c9f803f335edee41c3f2f05f262
prvdnx <129741476+Prvdnx@users.noreply.github>
2024-11-19 11:37:21 +0700

    Refactor wdps.sh script to improve readability; comment out filesystem permission fixes and WP configuration settings

--- srcs/requirements/wordpress/tools/wdps.sh
+++ srcs/requirements/wordpress/tools/wdps.sh
@@ -4,7 +4,6 @@ sleep 5
 
 if [ ! -f "/var/www/html/wp-config.php" ]; then
 	if cd /var/www/html && wp core download --allow-root
-
 	then
 	wp config create --allow-root \
 		--path=/var/www/html/ --dbname=$DB_NAME --dbuser=$DB_ADMIN \
@@ -21,14 +20,18 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 	fi
 fi
 
-# to allow WP write to filesystem directly without requirin FTP credentials: add FS_METHOD to wp-config.php
-if ! grep -q "FS_METHOD" /var/www/html/wp-config.php; then
-    echo "define('FS_METHOD', 'direct');" >> /var/www/html/wp-config.php
-fi
-
-# to fix Theme install error "Could not create directory. /var/www/html/wp-content/upgrade": set correct permissions
-chown -R www-data:www-data /var/www/html
-find /var/www/html -type d -exec chmod 755 {} \;
-find /var/www/html -type f -exec chmod 644 {} \;
-
 exec "$@"
+
+
+
+# docker exec -it wordpress-site bash
+
+# # to allow WP write to filesystem directly without requirin FTP credentials: add FS_METHOD to wp-config.php
+# if ! grep -q "FS_METHOD" /var/www/html/wp-config.php; then
+#     echo "define('FS_METHOD', 'direct');" >> /var/www/html/wp-config.php
+# fi
+
+# # to fix Theme install error "Could not create directory. /var/www/html/wp-content/upgrade": set correct permissions
+# chown -R www-data:www-data /var/www/html
+# find /var/www/html -type d -exec chmod 755 {} \;
+# find /var/www/html -type f -exec chmod 644 {} \;
